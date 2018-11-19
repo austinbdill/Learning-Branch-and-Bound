58843287fcb66a501afce60838d2dc3fa5a46395
Austin Dill <abdill@Austins-MacBook-Pro.local>
2018-11-18 23:27:20 -0500

    Added framework for branching

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import defaultdict
+import queue
 
 class Variable:
     
@@ -8,21 +8,68 @@ class Variable:
         self.y = y
         self.cluster = None
         
-    def set_cluster(cluster):
+    def __str__(self):
+        return str((self.x, self.y))
+        
+    def set_cluster(self, cluster):
         self.cluster = cluster
         
 class Cluster:
     
     def __init__(self):
         self.center = None
+        self.points = []
         
-    def set_center(center):
-        self.center = center
-
-def cluster_branch(points, k=5, depth_limit=None):
+    def calc_center(self):
+        x = np.mean([i.x for i in self.points])
+        y = np.mean([i.y for i in self.points])
+        self.center = (x, y)
+        
+    def add_point(self, point):
+        self.points.append(point)
+        
+    def reset_points(self):
+        self.points = []
+        
+def next_variable(variables, method="naive"):
+    if len(variables) == 0:
+        return None
+    if method == 'naive':
+        return variables[0]
+    else:
+        return None
+    
+def cost():
+    cost = 0
+    return cost
+        
+def cluster_branch(points, k=5, depth_limit=-1):
+    
+    #Initialize variable objects
     variables = []
-    clusters = defaultdict()
     for i in range(points.shape[0]):
-        point = points[0]
+        point = points[i]
         next_var = Variable(point[0], point[1])
+        variables.append(next_var)
+        
+   #Initialize clutser objects
+    clusters = []
+    for i in range(k):
+        clusters.append(Cluster())
+    
+    #Initialize helper variables
+    n_recurs = 0
+    q = queue.Queue()
+    q.put(next_variable(variables))
+    possible_cluster = dict()
+    for var in variables:
+        cluster_list 
+    
+    
+    #Clustering Loop
+    while (n_recurs != depth_limit and not q.empty()):
+        print(q.get())
+        
+        n_recurs = n_recurs + 1
+        
     return clusters
